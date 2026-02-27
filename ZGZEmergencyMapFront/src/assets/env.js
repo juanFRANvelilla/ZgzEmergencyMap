8b52236185189176acb7f15fef07083afadb7668
juanFRANvelilla <juanfran01velilla@gmail.com>
2026-02-27 15:15:00 +0100

    add env.js

---
+++ ZGZEmergencyMapFront/src/assets/env.js
@@ -0,0 +1,6 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // Variables de entorno por defecto para desarrollo local
+  window.__env.API_URL = 'http://localhost:8081';
+})(this);
